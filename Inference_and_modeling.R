9c61805c6d71c8305b3188be9fb466d93cbfd9e9
OliverRevilla <oliver.revilla@pucp.pe>
2021-02-18 22:37:43 -0500

    Commit for the practice part of the second unit

--- Inference_and_modeling.R
+++ Inference_and_modeling.R
@@ -1,3 +1,11 @@
+#Polls
+library(tidyverse)
+library(dslabs)
+take_poll(25)
+
+
+
+
 #Central Limit Theorem in practice
 
 x_hat <- 0.48 
@@ -61,9 +69,121 @@ data.frame(p = p, SE = SE) %>% ggplot(aes(p, SE)) +
   geom_line()
 
 
+#Exercises
+# Write a function called `take_sample` that takes `p` and `N` as arguements and returns the average value of a randomly sampled population.
+take_sample <- function(p,N){
+  sample <- sample(c(0,1),size = N, replace = TRUE, prob = c(1-p, p))
+  mean(sample)
+}
 
+# Use the `set.seed` function to make sure your answer matches the expected result after random sampling
+set.seed(1)
 
+# Define `p` as the proportion of Democrats in the population being polled
+p <- 0.45
 
+# Define `N` as the number of people polled
+N <- 100
+
+# Call the `take_sample` function to determine the sample average of `N` randomly selected people from a population containing a proportion of Democrats equal to `p`. Print this value to the console.
+take_sample(p,N)
+
+# Define `p` as the proportion of Democrats in the population being polled
+p <- 0.45
+
+# Define `N` as the number of people polled
+N <- 100
+
+# The variable `B` specifies the number of times we want the sample to be replicated
+B <- 10000
+
+# Use the `set.seed` function to make sure your answer matches the expected result after random sampling
+set.seed(1)
+
+# Create an objected called `errors` that replicates subtracting the result of the `take_sample` function from `p` for `B` replications
+errors <- replicate(B,{
+p - take_sample(p,N)
+})
+
+# Calculate the mean of the errors. Print this value to the console.
+mean(errors)
+hist(x = errors, nclass = 10)
+
+# Define `p` as the proportion of Democrats in the population being polled
+p <- 0.45
+
+# Define `N` as the number of people polled
+N <- 100
+
+# The variable `B` specifies the number of times we want the sample to be replicated
+B <- 10000
+
+# Use the `set.seed` function to make sure your answer matches the expected result after random sampling
+set.seed(1)
+
+# We generated `errors` by subtracting the estimate from the actual proportion of Democratic voters
+errors <- replicate(B, p - take_sample(p, N))
+
+# Calculate the mean of the absolute value of each simulated error. Print this value to the console.
+mean(abs(errors))
+
+#4.-
+# Define `p` as the proportion of Democrats in the population being polled
+p <- 0.45
+
+# Define `N` as the number of people polled
+N <- 100
+
+# The variable `B` specifies the number of times we want the sample to be replicated
+B <- 10000
+
+# Use the `set.seed` function to make sure your answer matches the expected result after random sampling
+set.seed(1)
+
+# We generated `errors` by subtracting the estimate from the actual proportion of Democratic voters
+errors <- replicate(B, p - take_sample(p, N))
+
+# Calculate the standard deviation of `errors`
+errors^2
+mean(errors^2)
+sqrt(mean(errors^2))
+
+#5-- Standard Error
+# Define `p` as the expected value equal to 0.45
+p <- 0.45
+
+# Define `N` as the sample size
+N <- 100
+
+# Calculate the standard error
+sqrt(p*(1-p)/N)
+
+#6.-
+# Define `p` as a proportion of Democratic voters to simulate
+p <- 0.45
+
+# Define `N` as the sample size
+N <- 100
+
+# Use the `set.seed` function to make sure your answer matches the expected result after random sampling
+set.seed(1)
+
+# Define `X` as a random sample of `N` voters with a probability of picking a Democrat ('1') equal to `p`
+X <- sample(c(0,1), size = N, prob = c(1-p,p), replace = TRUE)
+
+# Define `X_bar` as the average sampled proportion
+X_bar <- mean(X)
+
+# Calculate the standard error of the estimate. Print the result to the console.
+sqrt(X_bar*(1-X_bar)/N)
+
+#8.-
+N <- seq(100, 5000, len = 100)
+p <- 0.5
+se <- sqrt(p*(1-p)/N)
+plot(N,se)
+
+#9.- 
 
 
 
